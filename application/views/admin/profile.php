0d2940a7e3cdfeafc7666c113b42749fa75ad2f8
imaduddinaf <pentol231094@gmail.com>
2015-03-26 15:49:56 +0700

    (backend) edit profile fix

--- application/views/admin/profile.php
+++ application/views/admin/profile.php
@@ -39,7 +39,6 @@
 								</div>
 								<?php } ?>
 								<form class="form-horizontal row-border" action="<?php echo base_url(); ?>index.php/office/editprofile" method="post" enctype="multipart/form-data">
-								<div id="galery-input">
 									<div class="form-group">
 										<label class="col-md-2 control-label">Logo :</label>
 										<div class="col-md-10">
@@ -55,25 +54,25 @@
 									<div class="form-group">
 										<label class="col-md-2 control-label">Sambutan Ketua :</label>
 										<div class="col-md-10">
-											<textarea name="sambutan" cols="5" rows="5" class="form-control"><?php echo stripslashes($front->sambutan); ?></textarea>
+											<textarea name="sambutan" cols="5" rows="5" class="form-control wysiwyg"><?php echo stripslashes($front->sambutan); ?></textarea>
 										</div>
 									</div>
 									<div class="form-group">
 										<label class="col-md-2 control-label">Visi :</label>
 										<div class="col-md-10">
-											<textarea name="visi" cols="5" rows="5" class="form-control"><?php echo stripslashes($front->visi); ?></textarea>
+											<textarea name="visi" cols="5" rows="5" class="form-control wysiwyg"><?php echo stripslashes($front->visi); ?></textarea>
 										</div>
 									</div>
 									<div class="form-group">
 										<label class="col-md-2 control-label">Misi :</label>
 										<div class="col-md-10">
-											<textarea name="misi" cols="5" rows="5" class="form-control"><?php echo stripslashes($front->misi); ?></textarea>
+											<textarea name="misi" cols="5" rows="5" class="form-control wysiwyg"><?php echo stripslashes($front->misi); ?></textarea>
 										</div>
 									</div>
 									<div class="form-group">
 										<label class="col-md-2 control-label">Sejarah :</label>
 										<div class="col-md-10">
-											<textarea name="sejarah" cols="5" rows="5" class="form-control"><?php echo stripslashes($front->sejarah); ?></textarea>
+											<textarea name="sejarah" cols="5" rows="5" class="form-control wysiwyg"><?php echo stripslashes($front->sejarah); ?></textarea>
 										</div>
 									</div>
 									<div class="form-group">
@@ -88,7 +87,6 @@
 											<img src="<?php echo base_url(); ?>content/profil/<?php echo $front->organigram?>" class="img-responsive">
 										</div>
 									</div>
-								</div>
 									<div class="form-actions">
 										<label class="col-md-2 control-label"></label>
 										<button type="submit" class="submit btn btn-primary">
@@ -102,6 +100,19 @@
 				</div>				
 			</div>
 		</div>
+
+	<script type="text/javascript" src="<?php echo base_url(); ?>admin/plugins/typeahead/typeahead.min.js"></script> <!-- AutoComplete -->
+	<script type="text/javascript" src="<?php echo base_url(); ?>admin/plugins/autosize/jquery.autosize.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>admin/plugins/inputlimiter/jquery.inputlimiter.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>admin/plugins/uniform/jquery.uniform.min.js"></script> <!-- Styled radio and checkboxes -->
+	<script type="text/javascript" src="<?php echo base_url(); ?>admin/plugins/tagsinput/jquery.tagsinput.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>admin/plugins/select2/select2.min.js"></script> <!-- Styled select boxes -->
+	<script type="text/javascript" src="<?php echo base_url(); ?>admin/plugins/fileinput/fileinput.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>admin/plugins/duallistbox/jquery.duallistbox.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>admin/plugins/bootstrap-inputmask/jquery.inputmask.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>admin/plugins/bootstrap-wysihtml5/wysihtml5.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url(); ?>admin/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.min.js"></script>
+
 
 	
 
